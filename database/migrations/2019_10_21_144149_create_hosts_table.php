c972c1936b9f789a81528076eafe156f9cca69bf
Albert László <albert.laszlo@minic.ro>
2019-10-21 18:17:47 +0300

    hosts migration

---
+++ database/migrations/2019_10_21_144149_create_hosts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hosts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('logo');
+            $table->text('description');
+            $table->string('city');
+            $table->string('street');
+            $table->string('nr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hosts');
+    }
+}
