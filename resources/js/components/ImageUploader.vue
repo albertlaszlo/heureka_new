78a9106c2480594ee3af0d2892fd794c6d3a15f7
Gagyi Lori <lordlorenzo02@gmail.com>
2019-11-20 18:15:48 +0100

    message

---
+++ resources/js/components/ImageUploader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <label></label>
+    <input type="file" @change="onFileChanged"/>
+    <button>RemoveImage</button>
+    <br />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+props: [
+    "name"
+],
+
+  methods: {
+    async onFileChanged(event) {
+      const formData = new FormData();
+      formData.append(this.name, event.target.files[0]);
+      const response = await axios.post(
+        "http://127.0.0.1:8000/fileUpload",
+        formData
+      );
+      this.form.logo = response.data.path;
+    }
+  }
+};
+</script>
